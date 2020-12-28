4a2d0836ece15e890b607841b17cc9ef3dc4cb15
alberto <sinigagliaalberto@gmail.com>
2020-12-28 21:30:17 +0100

    fix paginazione

--- admin/product/index.php
+++ admin/product/index.php
@@ -4,7 +4,7 @@ redirectIfNotLogged();
 $page = file_get_contents('../template_html/product/index.html');
 $products = "";
 $cur_page = preg_match('/^[0-9]+$/', $_REQUEST['page']?? '') ? $_REQUEST['page'] : 0;
-$per_page = 20;
+$per_page = 8;
 $stm = DBC::getInstance()->prepare('SELECT * FROM PRODUCTS ORDER BY _ID LIMIT :limit OFFSET :offset');
 $stm->bindValue(':limit', (int) $per_page, PDO::PARAM_INT); 
 $stm->bindValue(':offset', (int) $cur_page * $per_page, PDO::PARAM_INT); 
@@ -47,13 +47,16 @@ foreach(($stm->fetchAll() ?? []) as $prod){
     ';
 }
 $page = str_replace('<products/>', $products, $page);
+
+
+
 $sql = "SELECT count(*) FROM PRODUCTS"; 
 $number_of_products = DBC::getInstance()->query($sql)->fetchColumn();
 
 $pagination = "";
 if($number_of_products > $per_page){
-    $last = ceil($number_of_products / $per_page);
-    if($cur_page != 0){
+    $last = ceil($number_of_products / $per_page) - 1;
+    if($cur_page > 0){
         $pagination = '
         <a class="button" title="Vai alla prima pagina" href="/admin/product/index.php?page=0">
             &lt;&lt; Prima
@@ -62,7 +65,7 @@ if($number_of_products > $per_page){
             &lt; <abbr title="Pagina precedente">Prec.</abbr>
         </a>';
     }
-    if($cur_page != $last){
+    if($cur_page < $last){
         $pagination.= '
         <a class="button" title="Vai alla pagina successiva" href="/admin/product/index.php?page='.($cur_page+1).'">
             <abbr title="Pagina successiva">Succ.</abbr> &gt; 
