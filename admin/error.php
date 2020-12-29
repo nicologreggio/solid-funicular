59c1f76ee006493ad77c1f910ec84c2fbe4a5204
alberto <sinigagliaalberto@gmail.com>
2020-12-29 15:05:43 +0100

    fix errore pagina errore perchè non partiva sessione

--- admin/error.php
+++ admin/error.php
@@ -1,4 +1,5 @@
 <?php
+require_once(__DIR__.'/inc/header_php.php');
 $page = file_get_contents('./template_html/error.html');
 echo str_replace("<error-message/>", $_SESSION['admin.error'] ??  "C'è stato un errore, la pagina cercata potrebbe essere stata rimossa o spostata", $page);
 if(isset($_SESSION['admin.error'])){
