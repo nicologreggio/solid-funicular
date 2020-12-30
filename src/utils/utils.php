3de98227f1490f35c5e042a2e9186f36cdb5b2fa
FilippoFantinato <fantinato.filippo99@gmail.com>
2020-12-31 00:37:32 +0100

    Developed login and signup

--- src/utils/utils.php
+++ src/utils/utils.php
@@ -3,12 +3,20 @@
 function fillPageWithError($page, $err)
 {
     $page = str_replace('<error-db/>', "", $page);
-    foreach($err as $k => $errors){
+    
+    foreach($err as $k => $errors)
+    {
         $msg = "<ul class='errors-list'>";
-        foreach($errors as $error){
+    
+        
+        foreach($errors as $error)
+        {
             $msg .= "<li> $error </li>";
         }
+        
         $msg .= "</ul>";
         $page = str_replace("<error-$k/>", $msg, $page);
     }
+
+    return $page;
 }
