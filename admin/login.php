a0a3968e65b209fbe25c693838712a4975efc75c
alberto <sinigagliaalberto@gmail.com>
2020-12-30 21:59:59 +0100

    aggiunto messaggio errore login fallito utente non trovato

--- admin/login.php
+++ admin/login.php
@@ -1,9 +1,13 @@
 <?php
 require_once(__DIR__.'/inc/header_php.php');
 redirectIfLogged();
+$page = file_get_contents('template_html/login.html');
 if($_SERVER['REQUEST_METHOD'] == 'POST' ){
     if(auth()->login($_POST['email'], $_POST['password']) === true){
         redirectIfLogged();
     }
+    else {
+        $page = str_replace('<error-login/>', '<p class="error">L\' cercato non è stato trovato</p>', $page);
+    }
 }
-echo file_get_contents('template_html/login.html');
+echo $page;
