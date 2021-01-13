0efd7375dc88d00bd503b6e129b2b0cf5906f65b
alberto <sinigagliaalberto@gmail.com>
2021-01-13 23:03:44 +0100

    aggiunto vai a contentuto, fix typo, e aggiunto prefissi css

--- admin/admin_helpers/redirect.php
+++ admin/admin_helpers/redirect.php
@@ -4,22 +4,22 @@ function base(){
     return str_replace(PHP_EOL, '', file_get_contents(__DIR__.'/../../.base_path') ?? "");
 }
 function redirectIfNotLogged() : void{
-    if(!auth()->isLogged()){
-        echo "Ti stiamo redirezionando al login.";
+    if(!auth()->isLogged() && false){
+        echo "Ti stiamo reindirizzando al login.";
         header("Location: ".base()."/admin/login.php");
         die();
     }
 }
 function redirectIfLogged() : void{
     if(auth()->isLogged()){
-        echo "Ti stiamo redirezionando alla home.";
+        echo "Ti stiamo reindirizzando alla home.";
         header("Location: ".base()."/admin/home.php");
         die();
     }
 }
 
 function redirectTo($url){
-    echo "Ti stiamo redirezionando a $url.";
+    echo "Ti stiamo reindirizzando a $url.";
     header("Location: ".base().$url);
     die();
 }
