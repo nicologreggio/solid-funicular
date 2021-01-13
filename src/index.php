2e7f030ad1a091ab3708615af79d517dd907d534
Nicolo Greggio - Ubuntu 20.04 <nico.greggio@gmail.com>
2021-01-13 14:05:18 +0100

    setting up categories dynamic

--- src/index.php
+++ src/index.php
@@ -1,22 +1,9 @@
 <?php
 
-function fetchCategories()
-{
-    //do the real db job
-    $categories = ["La Categoria 1", "Categoria num 2", "Terza Categoria", "4 di numero", "L'ultima si, la 5"];
-
-    $home=file_get_contents('./index.html');
-    foreach ($categories as $idx => $cat) {
-        $home=str_replace(("<cat-" . ($idx+1) . "/>"), $cat, $home);
-        //echo "<cat-" . ($idx+1) . "/>";
-    }
-
-    echo $home;
-
-}
+include './utils.php';
 
 
-fetchCategories();
+echo fetchAndFillCategories(file_get_contents('./index.html'));
 
 
 ?>
