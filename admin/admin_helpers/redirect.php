14ad81e53ee8ce43e7cd6725ceb8d96cd44bbdd9
alberto <sinigagliaalberto@gmail.com>
2021-01-13 18:31:43 +0100

    prova fix redirect header

--- admin/admin_helpers/redirect.php
+++ admin/admin_helpers/redirect.php
@@ -1,22 +1,25 @@
 <?php
 require_once(__DIR__.'/../inc/imports.php');
-function redirectIfNotLogged() : void{
+function base(){
+    return file_get_contents('../../.base_path') ?? "";
+}
+function redirectIfNotLogged($path_to_root) : void{
     if(!auth()->isLogged()){
         echo "Ti stiamo redirezionando al login.";
-        header("Location: /admin/login.php");
+        header("Location: ".base()."/admin/login.php");
         die();
     }
 }
 function redirectIfLogged() : void{
     if(auth()->isLogged()){
         echo "Ti stiamo redirezionando alla home.";
-        header("Location: /admin/home.php");
+        header("Location: ".base()."/admin/home.php");
         die();
     }
 }
 
 function redirectTo($url){
     echo "Ti stiamo redirezionando a $url.";
-    header("Location: $url");
+    header("Location: ".base().$url);
     die();
 }
