5ced1973c6ed4a31f9b4f37b1e8b33f8b556e635
alberto <sinigagliaalberto@gmail.com>
2021-01-14 01:13:16 +0100

    fix salvataggio foto

--- admin/admin_helpers/filesystem.php
+++ admin/admin_helpers/filesystem.php
@@ -4,10 +4,10 @@ function saveFromRequest($key){
         $name = $_FILES[$key]["name"];
         $junk = explode(".", $name);
         $ext = end($junk);
-        $loc = '../../images/products/'.uniqid().'.'.$ext;
+        $loc = '/images/products/'.uniqid().'.'.$ext;
         if (!move_uploaded_file(
             $_FILES[$key]['tmp_name'],
-            $loc
+            '../..'.$loc
         )) {
             return false;
         }
