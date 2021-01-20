9e185fa5849e101beee0d77c2ee33c980eeb42d6
Nicolo Greggio - Ubuntu 20.04 <nico.greggio@gmail.com>
2021-01-20 20:11:13 +0100

    added missing part to search, fixed behaviour, styling it

--- src/search.php
+++ src/search.php
@@ -8,7 +8,7 @@ require_once(__DIR__."/php/product/product.service.php");
 function fillSearchbar($page)
 {
     $searchbarStr = "
-        <label for='searchbar'>Cerca per nome:</label><br />
+        <label for='searchbar'>Termine di ricerca:</label><br />
         <input id='searchbar' name='searchbar' value='{$_SESSION['search']['name']}' />
     ";
 
@@ -20,7 +20,7 @@ function fillSearchbar($page)
 function fillCategories($page)
 {
     $categoriesStr = "
-        <label for='categories'>Cerca per categoria:</label><br />
+        <label for='categories'>Categoria:</label><br />
         <select id='categories' name='category'>
             <option value=''>Seleziona una categoria</option>
     ";
@@ -48,7 +48,8 @@ function fillCategories($page)
 
 function fillMaterials($page)
 {
-    $materialsStr = "<fieldset id='search-materials'><legend>Cerca per materiali:</legend>";
+    // $materialsStr = "<fieldset id='search-materials'><legend>Cerca per materiali:</legend>";
+    $materialsStr="";
 
     $materials = MaterialService::getAll();
 
@@ -56,7 +57,7 @@ function fillMaterials($page)
 
     foreach($materials as $index => $material)
     {
-        $materialsStr .= "<span class='search-material'><input id='search-material-{$index}' type='checkbox' name='materials[]' value='{$material->getId()}' ";
+        $materialsStr .= "<p class='search-material'><input id='search-material-{$index}' type='checkbox' name='materials[]' value='{$material->getId()}' ";
 
         if($searchedMaterials)
         {
@@ -66,7 +67,7 @@ function fillMaterials($page)
             }
         }
         
-        $materialsStr .= "/><label for='search-material-{$index}'>{$material->getName()}</label></span>";
+        $materialsStr .= "/><label for='search-material-{$index}'>{$material->getName()}</label></p>";
     
         if($index + 1 != count($materials))
         {
@@ -109,7 +110,7 @@ function fillPagination($page)
 {
     $limit = 15;
     $count = ProductService::getProductsListCount($_SESSION['search']);
-    echo $count;
+    // echo $count;
  
     $paginationStr = "
         <fieldset id='pagination'>
@@ -147,4 +148,8 @@ $_SESSION['search'] = [
     'page' => ($_GET['page']) ? (int) ($_GET['page']) : 1
 ];
 
-echo fillPage(file_get_contents("./search/search.html"));
+$page=file_get_contents("./search/search.html");
+$page=fetchAndFillCategories($page);
+$page=fillPage($page);
+
+echo $page;
