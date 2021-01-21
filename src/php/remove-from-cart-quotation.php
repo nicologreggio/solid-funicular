d28bc79fe3e001070b6f3fef7d3866909c5a604c
FilippoFantinato <fantinato.filippo99@gmail.com>
2021-01-21 18:19:26 +0100

    A

--- src/php/remove-from-cart-quotation.php
+++ src/php/remove-from-cart-quotation.php
@@ -3,9 +3,13 @@ session_start();
 
 if($_SERVER['REQUEST_METHOD'] == 'POST')
 {
-    $productId = $_SESSION['actual-product-id'];
+    $productId = $_POST['product-id'] ?? $_SESSION['actual-product-id'];
 
-    unset($_SESSION['cart'][$productId]);
+    echo $productId;
+
+    var_dump($_SESSION['cart']);
+
+    unset($_SESSION['cart'][(int) $productId]);
 
     header('Location: ' . $_SERVER['HTTP_REFERER']);
 }
