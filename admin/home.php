c5626c827b8f19ce506525904b326fa2ada53f20
Alberto Sinigaglia <sinigagliaalberto@gmail.com>
2021-01-25 14:13:34 +0100

    aggiunto messaggio inserimento amministratore

--- admin/home.php
+++ admin/home.php
@@ -1,4 +1,4 @@
 <?php
 require_once(__DIR__.'/inc/header_php.php');
 redirectIfNotLogged();
-echo str_replace('<username/>', auth()->user()->_NAME, file_get_contents('template_html/home.html'));
+echo str_replace('<username/>', auth()->user()->_NAME, page('template_html/home.html'));
