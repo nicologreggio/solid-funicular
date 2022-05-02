b3044df159cd62593a4f1be1be1406c6bf158d25
Nicolò Greggio <nicolo@moku.io>
2022-05-02 10:57:05 +0200

    adds required users for concorso

---
+++ database/2022-05-02_AddRequiredUsers.sql
@@ -0,0 +1,3 @@
+INSERT INTO `USERS` (`_ID`, `_NAME`, `_SURNAME`, `_CITY`, `_ADDRESS`, `_CAP`, `_ADMIN`, `_EMAIL`, `_PASSWORD`) VALUES
+(4, 'Super', 'Admin', 'Padua', 'Via Trieste', '35131', b'1', 'admin', 'admin'),
+(5, 'Piergiuseppe', 'Amadori', 'Gardaland', 'Via lattea', '39593', b'0', 'utente', 'utente');
